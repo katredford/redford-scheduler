473e3b0435c0a750336cb8baa87dc82cc7e039e3
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-04-03 16:52:35 -0500

    fix button divs

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,44 @@
 // https://day.js.org/docs/en/display/format
 
-// const timeDisplay = function () {
-//     var todaysDate = dayjs();
-//     var showTime = document.getElementById("currentDay");
+const timeDisplay = function () {
+    var todaysDate = moment()._d;
+    var showTime = document.querySelector("#currentDay");
 
-//     showTime.textContent = todaysDate;
-//     console.log(moment())
-//     console.log(todaysDate)
+    showTime.textContent = todaysDate;
+  
 
+};
+
+const displayTimeBlocks = function() {
+    var currentTime = moment()._d;
+    console.log(moment(currentTime).hour())//grabs current time hour
+   
+    const textAreas = document.querySelectorAll(".hour"); 
+    console.log(textAreas[0]);
     
+    const textAreahour = textAreas[0]
+    const currentTimeHour = moment(currentTime).hour();
 
-// };
+    for (let i = 0; i < textAreas.length; i++) {
+        const hourEl = textAreas[i]
+        var hourstring = hourEl.getAttribute("data-time")
+        console.log(hourstring)
 
-// timeDisplay()
+        const hour = parseInt(hourstring)
+        
+        if (hour > currentTimeHour) {
+            console.log(hour + " is after current time.");
+        } else if (hour < currentTimeHour) {
+            const el = document.getElementById("textarea-" + hour);
+            console.log(hour + " is before the current time.");
+            console.log("mark")
 
-const dayJsObject = dayjs();
-console.log(dayJsObject.format)
+        } else {
+            console.log(hour + " is the current hour.")
+        }
+    }
+}
+
+
+timeDisplay()
+displayTimeBlocks()
