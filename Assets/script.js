58996f5f4c5b48f5109502f2673bfc36a5e1bea0
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-04-03 11:12:02 -0500

    rename  assets

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,13 @@
 // https://day.js.org/docs/en/display/format
+
+const timeDisplay = function () {
+    var todaysDate = moment()._d;
+    var showTime = document.querySelector("#currentDay");
+
+    showTime.textContent = todaysDate;
+    console.log(moment())
+    console.log(todaysDate)
+
+    bleep
+
+};
